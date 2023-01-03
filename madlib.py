e9fffcfd3a41ef2c2ec54fafca1e51962c52d8b6
Khaled Elhannat <khaled.waleed1631@gmail.com>
2023-01-03 15:39:59 +0200

    initiat commit

---
+++ madlib.py
@@ -0,0 +1,11 @@
+adj = input('Additive: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+fam_per = input('Famous Person: ')
+
+
+madlib = f'Computer programming is so {adj}! It make me excited all the time because \
+I love {verb1}. Stay hydrated and {verb2} like you are {fam_per}'
+
+
+print(madlib) 
